e08c2e7cb2af2cecc0f71a198b112ecb971da85f
gbachik <gbachik@gmail.com>
2014-10-20 06:16:39 -0600

    sessons and login + logout working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,28 @@
 class UsersController < ApplicationController
-	def new
-		@user = User.new
-	end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			# Handle a successful save.
-			flash[:success] = "Welcome to B-Trade!"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
+  def new
+    @user = User.new
+  end
 
-	private
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
-		def user_params
-			params.require(:user).permit(:name, :email, :password,
-																	 :password_confirmation)
-		end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
